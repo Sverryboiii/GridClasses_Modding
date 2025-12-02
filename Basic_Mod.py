5376e38a1afd552bdc067a3379d99ccc1d2cb54a
Sverryboiii <sverreplaysgoodbs@gmail.com>
2025-12-02 21:21:20 +0100

    Simple example of a GridClasses Mod

---
+++ Basic_Mod.py
@@ -0,0 +1,27 @@
+"""
+This is just for a vision of what you could do with modding.
+Here you gain a mod_score for each time stepped on a different grass tile.
+After going through the door you claim your normal score + mod_score * 1000!
+"""
+
+Mod_Score = 0
+Claimed_tiles = []
+
+def Update_Frame(data):
+  global Mod_Score
+  text = data.font.render(f"Mod score: {Mod_Score}", True, (255, 255, 255))
+  screen.blit(text, (10, 10
+
+def Tile0(data):
+  global Mod_Score
+  for tile in Claimed_tiles:
+    if data.new_pos == tile:
+      return True
+  Claimed_tiles.append(new_pos)
+  Mod_Score += 1
+  return True
+
+def Tile2(data):
+  global Mod_Score
+  Player.score += 1000*Mod_Score
+  return True
