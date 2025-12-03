773a53af6d3464d747716560637523bcb4e553e8
Sverryboiii <sverreplaysgoodbs@gmail.com>
2025-12-03 15:07:22 +0100

    Update Basic_Mod.py

--- Basic_Mod.py
+++ Basic_Mod.py
@@ -1,21 +1,28 @@
 """
 This is just for a vision of what you could do with modding.
-Here you gain a mod_score for each time stepped on a different grass tile.
-After going through the door you claim your normal score + mod_score * 1000!
-Your player health also decays by 1 every second.
+I made a mod that uses every aspect of the mod-loader
 """
 
 Mod_Score = 0
 Claimed_tiles = []
 
+# When updating scorches don't move, but give +1 hp
+def On_Enemy_UpdateScorch(data):
+  data.enemy.health += 1
+  return False
+
+# Every frame show the Mod score on screen
 def Frame_Updater(data):
   global Mod_Score
   text = data.font.render(f"Mod score: {Mod_Score}", True, (255, 255, 255))
   data.screen.blit(text, (10, 10))
 
+# Every second decrease the players health by 1
 def Second_Updater(data):
   Player.health -= 1
 
+# When the player steps on a grass tile and the position of that grass tile isn't registered
+# you get +1 mod score
 def Tile0(data):
   global Mod_Score
   for tile in Claimed_tiles:
@@ -25,6 +32,8 @@ def Tile0(data):
   Mod_Score += 1
   return True
 
+# When the player steps on a door tile the mod-score gets converted to normal score
+# (1000 score / 1 mod score)
 def Tile2(data):
   global Mod_Score
   Player.score += 1000*Mod_Score
